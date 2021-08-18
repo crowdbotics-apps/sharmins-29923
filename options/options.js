f94e8d862d55066185bcb5019f99639d456af5b6
Crowdbotics <team@crowdbotics.com>
2021-08-18 19:15:50 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "sharmins_29923Identifier",
+  url: "https://sharmins_29923Identifier.botics.co",
+  api: "https://sharmins_29923Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
